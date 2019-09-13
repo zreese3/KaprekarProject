32cf5b5a3a1590538516165bfb1d4a45f0c529de
Zak Reese <zreese3@huskers.unl.edu>
2019-09-13 12:36:19 -0500

    corrected spelling and equation
    Spell checked variable 'modulus' to match other one.
    Added another '=' to make the if statement valid

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -40,7 +40,7 @@ int isKaprekar(int n) {
   //for each possible "split" of the square...
   for(i=1; i<=numDigits; i++) {
     //increase the modulus by a factor of 10
-    modulous *= 10;
+    modulus *= 10;
 
     //split the square into two parts
     first = square / modulus;
@@ -49,7 +49,7 @@ int isKaprekar(int n) {
     //test if the split makes a Kaprekar number
     //the second part cannot be zero and if
     //the sum of the two parts is equal to n, then it is
-    if(second > 0 && n = first + second) {
+    if(second > 0 && n == first + second) {
       return 1;
     }
   }
